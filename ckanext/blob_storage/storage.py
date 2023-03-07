e0d21b130b279728659f3f86deaabb9876a30d49
Michal Ful <michal@fjelltopp.org>
2023-03-07 10:16:07 +0100

    Return none to iUploader

--- ckanext/blob_storage/storage.py
+++ ckanext/blob_storage/storage.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import cgi
-from datetime import datetime
-
-from ckan import model
-from ckan.lib import munge
-from .download_handler import download_handler
-
-
 class ResourceBlobStorage(object):
     def __init__(self, resource):
-        super(ResourceBlobStorage, self).__init__()
+        return None
 
     def get_path(self, id):
-        return download_handler(id)
+        return None
 
     def upload(self, id, max_size):
         return None
